0ec865e40abcf1826cddb829affa52cfb75c0f19
soenneker <jake@soenneker.com>
2026-09-03 22:02:45 -0500

    Add interface inheritdoc references

--- src/Soenneker.Ups.HttpClients/UpsOpenApiHttpClient.cs
+++ src/Soenneker.Ups.HttpClients/UpsOpenApiHttpClient.cs
@@ -11,6 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Ups.HttpClients;
 
+/// <inheritdoc cref="IUpsOpenApiHttpClient" />
 public sealed class UpsOpenApiHttpClient : IUpsOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
